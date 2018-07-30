3fd689a9c5cb82489fa293b567a5f478c0381534
matthew <mthwbrwn@gmail.com>
2018-07-29 20:23:46 -0700

    adds made to URL

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -4,7 +4,7 @@ var app = app || {};
 
 (function(module){
 
-  let productionApiUrl = 'insert cloud API server URL here';
+  let productionApiUrl = 'https://www.googleapis.com';
   let developmentApiUrl = 'http://localhost:3000';
 
   module.isProduction= /^(?!localhost|127)/.test(window.location.hostname);
